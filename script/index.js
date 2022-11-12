39e5217959fb563bc07233d4d62085104466e63a
ArtemMazin <minima1s@yandex.ru>
2022-11-12 23:58:50 +0300

    Добавил стили для первой карточки

--- script/index.js
+++ script/index.js
@@ -12,6 +12,7 @@ class Card {
 
   render() {
     const element = document.createElement("div");
+    element.className = "card"
     element.innerHTML = `
       <img class="card__image" src="${this.image}"></img>
       <div class="card__description">
@@ -34,5 +35,5 @@ new Card(
   'Rajgad Fort',
   'Rajgad is a hill fort in Pune. It was initially called Murumbdev and was later renamed to Rajgad by Shivaji Maharaj. It is located south-west to Pune City. The base village Gunjavane is around 60km from Pune. This fort was initially the capital of Shivaji Maharaj\'s Hindavi Swarajya.The capital was later shifted to Raigad.The area of the fort base is 24 square miles.',
   '#',
-  '.card'
+  '.main'
 ).render();
