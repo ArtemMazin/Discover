e7d47570c00e7c72e31a6d8e90881ffe0d987f35
ArtemMazin <minima1s@yandex.ru>
2022-11-13 15:40:03 +0300

    Добавил background-color и  стили в slide

--- script/index.js
+++ script/index.js
@@ -12,7 +12,7 @@ class Card {
 
   render() {
     const element = document.createElement("div");
-    element.className = "card"
+    element.className = "card";
     element.innerHTML = `
       <img class="card__image" src="${this.image}"></img>
       <div class="card__description">
@@ -34,6 +34,46 @@ new Card(
   'KING OF FORT',
   'Rajgad Fort',
   'Rajgad is a hill fort in Pune. It was initially called Murumbdev and was later renamed to Rajgad by Shivaji Maharaj. It is located south-west to Pune City. The base village Gunjavane is around 60km from Pune. This fort was initially the capital of Shivaji Maharaj\'s Hindavi Swarajya.The capital was later shifted to Raigad.The area of the fort base is 24 square miles.',
+  '#',
+  '.main'
+).render();
+
+
+class Slide {
+  constructor(image, chapter, title, text, link, parent) {
+    this.image = image
+    this.chapter = chapter
+    this.title = title
+    this.text = text
+    this.link = link
+    this.parent = document.querySelector(parent)
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.classList = "slide";
+    element.innerHTML = `
+    <img class="slide__image" src="${this.image}"></img>
+      <div class="slide__description">
+        <div class="chapter chapter_place_slide">
+          <span class="chapter__span"></span>
+          <p class="chapter__name">${this.chapter}</p>
+          <span class="chapter__span"></span>
+        </div>
+        <h2 class="slide__title">${this.title}</h2>
+        <p class="slide__text">${this.text}</p>
+        <a href="${this.link}" class="slide__link">read more &#8594;</a>
+      </div>
+    `;
+    this.parent.append(element);
+  }
+};
+
+new Slide (
+  '../images/Shreemant-Dagdusheth.png',
+  'Bromo',
+  'Shreemant Dagdusheth Halwai Ganpati Mandir',
+  'The Dagadusheth Halwai Ganapati temple in Pune is dedicated to the Hindu god Ganesh. The temple is visited by over hundred thousand pilgrims every year. Devotees of the temple include celebrities and chief ministers of Maharashtra who visit during the annual ten-day Ganeshotsav festival.',
   '#',
   '.main'
 ).render();
